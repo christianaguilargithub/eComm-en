5838c091ece0cb86561e0cda42673c3b0728ff58
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-06-15 15:14:18 +0800

    asfaF

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -16,6 +17,27 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
 
+
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding: 100px;
+    }
+</style>
+<script>
+    $.ajax({
+    type: "POST",
+    url: '/your_url',
+    data: { somefield: "Some field value", _token: '{{csrf_token()}}' },
+    success: function (data) {
+       console.log(data);
+    },
+    error: function (data, textStatus, errorThrown) {
+        console.log(data);
+
+    },
+});
+</script>
 </html>
